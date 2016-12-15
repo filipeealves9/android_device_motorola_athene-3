ashwinr64/android_device_motorola_athene

put ramdisk in the right directory

device.mk
@@ -186,8 +186,19 @@ PRODUCT_PACKAGES += \
     power.msm8952
 
 # Ramdisk
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,${LOCAL_PATH}/ramdisk,root)
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.mmi.boot.sh \
+    init.mmi.laser.sh \
+    init.mmi.touch.sh \
+    init.mmi.usb.rc \
+    init.oem.hw.sh \
+    init.qcom.sh.sh \
+    init.qcom.power.rc \
+    init.qcom.rc \
+    init.qcom.ril.sh \
+    ueventd.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \

filipeealves9/android_device_motorola_athene-3

device.mk
@@ -193,8 +193,19 @@ PRODUCT_PACKAGES += \
     power.msm8952
 
 # Ramdisk
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,${LOCAL_PATH}/ramdisk,root)
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.mmi.boot.sh \
+    init.mmi.laser.sh \
+    init.mmi.touch.sh \
+    init.mmi.usb.rc \
+    init.oem.hw.sh \
+    init.qcom.bt.sh \
+    init.qcom.power.rc \
+    init.qcom.rc \
+    init.qcom.ril.sh \
+    ueventd.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \